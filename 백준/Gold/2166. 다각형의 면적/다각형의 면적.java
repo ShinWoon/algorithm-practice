a82d70eae83d8a228c2204b26b053df6aa63d195
Shin Woon Hwang <76505735+ShinWoon@users.noreply.github.com>
2023-07-08 17:24:56 +0900

    [Gold V] Title: 다각형의 면적, Time: 204 ms, Memory: 20548 KB -BaekjoonHub

--- 백준/Gold/2166. 다각형의 면적/다각형의 면적.java
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 public class Main {
     private static String result;
     private static int N;
-    private static long[][] point;
+    private static long[][] point;  // 이게 문제였군,,,
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -19,8 +19,8 @@ public class Main {
             point[i][1] = y;
         }
 
-        point[N][0] = point[0][0];
-        point[N][1] = point[0][1];
+//        point[N][0] = point[0][0];
+//        point[N][1] = point[0][1];
 
         solve();
         System.out.println(result);
@@ -29,27 +29,28 @@ public class Main {
     private static void solve() {
         // 점들로 면적 구하는 공식
         // 1/2 * |(x1*y2 + x2*y3 + x3*y1) - (y1*x2 + y2*x3 + y3*x1)|
+        // 고정 값
+        long x = point[0][0];
+        long y = point[0][1];
+        long triSum = 0;
+        for(int i=1; i<N-1; i++) {
+            long x2 = point[i][0];
+            long y2 = point[i][1];
+            long x3 = point[i+1][0];
+            long y3 = point[i+1][1];
+            triSum += ((x*y2 + x2*y3 + x3*y) - (y*x2 + y2*x3 + y3*x));
+        }
+        
 
-//        // 고정 값
-//        int x = point[0][0];
-//        int y = point[0][1];
-//        long triSum = 0;
-//        for(int i=1; i<N-1; i++) {
-//            int x2 = point[i][0];
-//            int y2 = point[i][1];
-//            int x3 = point[i+1][0];
-//            int y3 = point[i+1][1];
-//            triSum += ((x*y2 + x2*y3 + x3*y) - (y*x2 + y2*x3 + y3*x)) * 0.5;
+//        long areaA = 0;
+//        long areaB = 0;
+//
+//        for(int i=0; i<N; i++) {
+//            areaA += point[i][0] * point[i+1][1];
+//            areaB += point[i+1][0] * point[i][1];
 //        }
 
-        long areaA = 0;
-        long areaB = 0;
-
-        for(int i=0; i<N; i++) {
-            areaA += point[i][0] * point[i+1][1];
-            areaB += point[i+1][0] * point[i][1];
-        }
-
-        result = String.format("%.1f", Math.abs(areaA-areaB)/2.0);
+//        result = String.format("%.1f", Math.abs(areaA-areaB)/2.0);
+        result = String.format("%.1f", Math.abs(triSum)/2.0);
     }
 }
