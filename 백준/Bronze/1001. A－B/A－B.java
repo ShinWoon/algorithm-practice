fa9355d988b1aab761fa106071ef749cb065ef04
Shin Woon Hwang <76505735+ShinWoon@users.noreply.github.com>
2023-12-03 00:35:19 +0900

    [Bronze V] Title: A-B, Time: 108 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int A, B;
+        
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+        System.out.println((A-B));
+    }
+}
